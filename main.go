10e92713b9170e1b47130333c445bc367b3a828b
Kevin A. Figueroa <2018117755@ub.edu.bz>
2023-02-13 13:13:46 -0600

    Anogther method of syncronization

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
 //filename: main.go
+//another way to syncronize your go routines
 
 package main
 
@@ -7,21 +8,26 @@ import (
 	"sync"
 )
 
+func one(wg *sync.WaitGroup) {
+	defer wg.Done()
+fmt.Println("hola")
+}
+func two(wg *sync.WaitGroup) {
+	defer wg.Done()
+fmt.Println("ni hao")
+}
+func three(wg *sync.WaitGroup) {
+	defer wg.Done()
+fmt.Println("hello")
+}
+
+
 func main() {
 	var wg sync.WaitGroup
 	wg.Add(3)
+	go one(&wg)
+	go two(&wg)
+	go three(&wg)
 
-	func() {
-		defer wg.Done()
-fmt.Println("hola")
-	}()
-	func() {
-		defer wg.Done()
-fmt.Println("ni hao")
-	}()
-	func() {
-		defer wg.Done()
-fmt.Println("hello")
-	}()
 	wg.Wait()//wait until wg becomes 0
 }
