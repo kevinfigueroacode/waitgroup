84d8530d67d02e7e561cc1afee50aa2fdbb65c35
Kevin A. Figueroa <2018117755@ub.edu.bz>
2023-02-15 13:09:47 -0600

    CAPS

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ package main
 import (
 "flag"
 "fmt"
-//"strings"
+"strings"
 )
 
 
@@ -16,8 +16,16 @@ func main() {
 //set the flags
 msg := flag.String("msg", "Howdy, stranger!", "the message to display")
 num := flag.Int("num",1 , "how many time to display the message")
-caps := flag.Bool("caps", false,"should the string print in caps")
+caps := flag.Bool("U", false,"specify whether to uppercase the string(true or false)")
+//caps := flag.Bool("caps", false,"should the string print in caps")
 flag.Parse()
+//check if it is upper case or if it should be uppercase before printing it.
+if *caps {
+*msg = strings.ToUpper(*msg)
+}
+//print the s string
+
+
 //check if the user set the flag
 //fmt.Println(*msg)
 //fmt.Println(*num)
